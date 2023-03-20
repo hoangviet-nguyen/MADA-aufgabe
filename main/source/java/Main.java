7f74f92f9d26ebf8f7b6bc71fcffe8ce707af89e
Viktor Weilenmann <viktor.weilenmann@students.fhnw.ch>
2023-03-20 14:55:00 +0100

    Keys now gets saved to txt file

--- main/source/java/Main.java
+++ main/source/java/Main.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.math.BigInteger;
 
 import java.util.List;
@@ -12,10 +15,43 @@ public class Main {
         BigInteger p = new BigInteger(2048, 5, rnd);
         BigInteger q = new BigInteger(2048, 5, rnd);
         BigInteger n = p.multiply(q);
+
+
         BigInteger phi = euklid.function(p, q);
         BigInteger e = euklid.possibleNumbers(phi);
         BigInteger d = euklid.erweitertGgt(phi,e);
         System.out.println(e);
         System.out.println(d);
+        File pathPrivate = new File("C:\\Users\\hloga\\IdeaProjects\\MADA-aufgabe\\sk.txt");
+        File pathPublic = new File("C:\\Users\\hloga\\IdeaProjects\\MADA-aufgabe\\pk.txt");
+        try { //probiert PrivateKey zu speichern
+            FileWriter wr = new FileWriter(pathPrivate);
+            wr.write(n.toString());
+            wr.write(d.toString());
+
+
+            System.out.println("test1");
+            wr.flush();
+            wr.close();
+
+        }  catch (IOException ee) {
+            System.out.println("fehler");
+            throw new RuntimeException(ee);
+
+        }
+        try {//probiert PublicKey zu speichern
+            FileWriter wr2 = new FileWriter(pathPublic);
+            wr2.write(n.toString());
+            wr2.write(e.toString());
+            wr2.flush();
+            wr2.close();
+        }catch (IOException ee) {
+            System.out.println("fehler");
+            throw new RuntimeException(ee);
+
+        }
+
+
+
     }
 }
