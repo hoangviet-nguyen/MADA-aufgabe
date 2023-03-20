98eb1f21f78843d13838a2076ffea594e555ca8c
Hoang Viet Nguyen <hoangviet.nguyen@students.fhnw.ch>
2023-03-20 13:35:56 +0100

    Erweiteter Algorithmus

--- main/source/java/Algorithmen/Euklid.java
+++ main/source/java/Algorithmen/Euklid.java
@@ -19,7 +19,7 @@ public class Euklid {
         return possible;
     }
 
-    public int easyGgt(int a, int b){
+    public int easyGgt(int a, int b) { // easy euklidian algorithm
         if (a < b){
             int temp = a;
             a = b;
@@ -33,5 +33,36 @@ public class Euklid {
         }
         return b;
     }
+
+    public  static int erweitertGgt(int m, int e) { // continued euklidian algorithm
+        int a = m; int b = e;
+        int x0 = 1; int y0 = 0; int x1 = 0; int y1 = 1;
+        int q = a / b; int r = a % b; int tempx; int tempy;
+        int[] bezout = new int[2];
+        while (b > 0) {
+            a = b;
+            b = r;
+            tempx = x0;
+            tempy = y0;
+            x0 = x1;
+            y0 = y1;
+            x1 = tempx - q * x1;
+            y1 = tempy - q * y1;
+            if (b == 0){
+                if (y0 < 0){
+                    if (x1 * m + y1 * e == 0) {
+                        return y1 + y0;
+                    } else {
+                        return y1;
+                    }
+                }
+                return y0;
+            }
+            q = a/b;
+            r = a % b;
+        }
+        return y0;
+    }
+
 
 }
