383a3c0fb89c617c086c1bc6a8c4acd0bf08420c
Viktor Weilenmann <viktor.weilenmann@students.fhnw.ch>
2023-03-24 09:10:04 +0100

    2. public key gets read from file

--- main/source/java/Algorithmen/main.java
+++ main/source/java/Algorithmen/main.java
@@ -1,11 +1,15 @@
 package Algorithmen;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.math.BigInteger;
 import java.util.Random;
+import java.util.Scanner;
 
 public class main {
 
     public static void main(String[] args) {
+        /*
         Algorithms algorithm = new Algorithms();
         Random rnd = new Random();
         BigInteger p = new BigInteger(2048, 8, rnd); // generiert 2x 2048 bit lange Primzahlen
@@ -19,5 +23,34 @@ public class main {
         BigInteger x = new BigInteger("6");
 
        // System.out.println(algorithm.fastModExpo(,e,n));
+
+         */
+
+        //liest public key von pk.txt
+        Scanner scanner = new Scanner(System.in);
+        Scanner reader;
+        String bigString = "";
+        try {
+            File textFile = new File("pk.txt");
+            reader = new Scanner(textFile);
+            scanner.useDelimiter(",");
+            int counter = 0;
+            while (reader.hasNextLine()) {
+
+                bigString = reader.nextLine();
+
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("fehler beim Lesen der Datei");
+            throw new RuntimeException(e);
+        }
+
+        String[] keys = bigString.substring(1,bigString.length()-1).split(",");
+        BigInteger n = new BigInteger(keys[0]);
+        BigInteger e = new BigInteger(keys[1]);
+        System.out.println(bigString);
+        System.out.println(keys[0]);
+        System.out.println(keys[1]);
+
     }
 }
--- main/source/java/Main.java
+++ main/source/java/Main.java
@@ -11,35 +11,36 @@ import Algorithmen.Algorithms;
 
 public class Main {
 
-    public static Algorithms algorithm;
+    public static Algorithms algorithm =  new Algorithms();
     public static BigInteger p;
     public static BigInteger q;
     public static BigInteger n;
     public static BigInteger phi;
     public static BigInteger e;
     public static BigInteger d;
+    public static Scanner scanner;
     public static ArrayList<BigInteger> verschluesselt = new ArrayList<>();
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        scanner = new Scanner(System.in);
         System.out.println("Bitte wählen Sie eine Option aus:");
-        System.out.println("1. Schlüsselgenerierung");                  //Aufgabe 1
-        System.out.println("2. Verschlüsselung");                       //Aufgabe 2
-        System.out.println("3. Entschlüsselung");                       //Aufgabe 3
+        System.out.println("1. Schlüsselgenerierung");              //Aufgabe 1
+        System.out.println("2. Verschlüsselung");                   //Aufgabe 2
+        System.out.println("3. Entschlüsselung");                   //Aufgabe 3
         int input = scanner.nextInt();
 
         if (input == 1) {
-            algorithm = new Algorithms();                               //erstellt Objekt der Klasse Euklid mit allen Rechenmethoden
+                                                                      //erstellt Objekt der Klasse Euklid mit allen Rechenmethoden
             Random rnd = new Random();
-            p = new BigInteger(1024, 8, rnd); // generiert 2x 2048 bit lange Primzahlen
-            q = new BigInteger(1024, 8, rnd); // mit certainty 8 ist die wahrscheinlichkeit 99.61% eine Primzahl
-            n = p.multiply(q);                               // berechnet n = p*q
+            p = new BigInteger(1024, 8, rnd);       // generiert 2x 2048 bit lange Primzahlen
+            q = new BigInteger(1024, 8, rnd);       // mit certainty 8 ist die wahrscheinlichkeit 99.61% eine Primzahl
+            n = p.multiply(q);                                      // berechnet n = p*q
 
 
-            phi = algorithm.function(p, q);                     // berechnet phi(n) = (p-1)*(q-1)
-            e = algorithm.possibleNumbers(phi);                 // berechnet mögliche e, die kleiner als phi(n) sind und mit phi(n) teilerfremd sind
-            d = algorithm.erweitertGgt(phi, e);                  // berechnet d mit dem erweiterten euklidischen Algorithmus
+            phi = algorithm.function(p, q);                         // berechnet phi(n) = (p-1)*(q-1)
+            e = algorithm.possibleNumbers(phi);                     // berechnet mögliche e, die kleiner als phi(n) sind und mit phi(n) teilerfremd sind
+            d = algorithm.erweitertGgt(phi, e);                     // berechnet d mit dem erweiterten euklidischen Algorithmus
 
             System.out.println("n: " + n);
             System.out.println("e: " + e);
@@ -65,21 +66,48 @@ public class Main {
             }
         }
         if (input == 2) {
-            System.out.println("TODO");
 
-            File textFile = new File("text.txt");
+            Scanner scanner = new Scanner(System.in);
             Scanner reader;
-            String text = "";
+            String bigString = "";
             try {
+                File textFile = new File("pk.txt");
                 reader = new Scanner(textFile);
+                scanner.useDelimiter(",");
+                int counter = 0;
+                while (reader.hasNextLine()) {
+
+                    bigString = reader.nextLine();
+
+                }
             } catch (FileNotFoundException e) {
                 System.out.println("fehler beim Lesen der Datei");
                 throw new RuntimeException(e);
             }
 
-            while (reader.hasNextLine()) {
-                text = reader.nextLine();
+            String[] keys = bigString.substring(1,bigString.length()-1).split(",");
+            BigInteger n = new BigInteger(keys[0]);
+            BigInteger e = new BigInteger(keys[1]);
+            String en = "";
+
+            File textFile = new File("text.txt");
+
+            String text = "";
+            try {
+                reader = new Scanner(textFile);
+                while (reader.hasNextLine()) {
+                    text = reader.nextLine();
+                }
+            } catch (FileNotFoundException ex) {
+                System.out.println("fehler beim Lesen der Datei");
+                throw new RuntimeException(ex);
             }
+
+
+
+
+
+
             for (int i = 0; i < text.length(); i++) {
                 System.out.println(text.charAt(i));                                // Liest die String werte aus dem Text
                 int temp = text.charAt(i);                                         // Wandelt die Werte in int's um
